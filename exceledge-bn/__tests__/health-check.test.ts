f6b38b4fb39d7d6b15576b57eb0b6c5854ae3a97
niyobertin <niyonkurubbertin@gmail.com>
2025-04-13 14:40:49 +0200

    new change for deploying on lender

--- exceledge-bn/__tests__/health-check.test.ts
+++ exceledge-bn/__tests__/health-check.test.ts
@@ -1,10 +1,18 @@
+// __tests__/health-check.test.ts
+
 import dotenv from "dotenv";
 dotenv.config({ path: ".env.test" });
-
 import request from "supertest";
 import { app } from "../src/app";
 
-import redisClient from "../src/utils/connectRedis";
+// Mock cron jobs
+jest.mock("node-cron", () => {
+  return {
+    schedule: jest.fn(() => ({
+      stop: jest.fn(),
+    })),
+  };
+});
 
 describe("Health Check", () => {
   it("should respond with success", async () => {
@@ -12,13 +20,9 @@ describe("Health Check", () => {
     expect(res.status).toBe(200);
     expect(res.body.status).toBe("success");
   });
-
-  it("should return a message from Redis", async () => {
-    await redisClient.set("try", "Redis is working!");
-    const res = await request(app).get("/api/v1/health-check");
-    expect(res.body.message).toBe("Redis is working!");
-  });
-  afterAll(async () => {
-    await redisClient.quit();
-  });
+});
+afterAll(() => {
+  setTimeout(() => {
+    process.exit(0);
+  }, 500);
 });

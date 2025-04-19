89a6eee4afdc15e2c01317e04176eac09218d112
niyobertin <niyonkurubbertin@gmail.com>
2025-04-19 23:43:37 +0200

    transactions status inidcator

--- exceledge-fn/src/components/common/dashboards/DashboardHome.tsx
+++ exceledge-fn/src/components/common/dashboards/DashboardHome.tsx
@@ -285,9 +285,9 @@ export const DashboardHome = () => {
                       <td className="px-4 py-4 whitespace-nowrap">
                         <span
                           className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-                            txn.status === "Completed"
+                            txn.status === "COMPLETED"
                               ? "bg-green-100 text-green-500"
-                              : txn.status === "Pending"
+                              : txn.status === "PENDING"
                               ? "bg-yellow-100 text-yellow-800"
                               : "bg-red-100 text-red-500"
                           }`}

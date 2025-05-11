cabe4694785ad6d7a8f53190d09ea47765cfdf1f
Niyonkuru Bertin <85437905+niyobertin@users.noreply.github.com>
2025-05-11 07:26:55 +0200

    Update server.ts

--- exceledge-bn/src/server.ts
+++ exceledge-bn/src/server.ts
@@ -4,13 +4,13 @@ import { Server } from "socket.io";
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
+export const io = new Server(server, {
   cors: {
     origin: ["https://exceledgecpa.com", "https://www.exceledgecpa.com"],
     methods: ["GET", "POST", "PATCH", "DELETE", "UPDATE"],
-    credentials: true
-  }
+  },
 });
+
 
 
 io.on("connection", (socket) => {
